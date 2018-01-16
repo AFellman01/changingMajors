3f26d0a615e9509011753f630d7930f26fbfa10a
Alexander Fellman <alexanderfellman@The-Wonderful-Wombat-of-Oz.local>
2018-01-16 14:52:24 -0500

    Router Start

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ var express = require("express");
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
 var logger = require("morgan");
-const apiroutes = require("./routes/apiroutes.js");
-
-const PORT = process.env.PORT || 3001;
 
 var app = express();
 
@@ -15,7 +12,6 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(express.static("public"));
-app.use(apiroutes);
 
 
 mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
@@ -28,7 +24,6 @@ db.once('open', function() {
   console.log("mongoose connection successful");
 });
 // Listen on port 3000
-app.listen(PORT, function() {
-  console.log("App running on " + PORT + "!");
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
 });
-
