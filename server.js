db30e1bf70c29f15018b8cc3cfa2377be7432ec0
Cory Lancaster <coryolivia@Corys-MacBook-Pro.local>
2018-01-17 02:26:21 -0500

    ch-ch-ch-changes..

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
-// Dependencies
-var express = require("express");
-var mongoose = require('mongoose');
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-const apiroutes = require("./routes/apiroutes.js");
+//Loading environmental variables here
+if (process.env.NODE_ENV !== 'production') {
+	console.log('loading dev environments')
+	require('dotenv').config()
+}
+require ('dotenv').config()
 
+// Dependencies
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const dbConnection = require('db') 
+const passport = require('./passport')
+const apiroutes = require("./routes/apiroutes.js");
+const socketIO = require('socket.io');
+const path = require('path');
+
+//Create the express app, the http server, and socket.io server
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const PORT = process.env.PORT || 3001;
 
-var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Middleware
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
+app.use(bodyParser.json())
+app.use(
+	session({
+		secret: process.env.APP_SECRET || 'this is the default passphrase',
+		store: new MongoStore ({ mongooseConnection: dbConnection}),
+		resave: false,
+		saveUninitialized: false
+
+	})
+
+)
 
 // /e/ Set the app up with morgan, body-parser, and a static folder
 app.use(logger("dev"));
@@ -27,6 +65,32 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("mongoose connection successful");
 });
+
+//Passport
+app.use(passport.initialize())
+app.use(passport.session())
+
+//Build
+if(process.env.NODE_ENV === 'production') {
+	const path = require('path')
+	console.log('YOU ARE IN THE PRODUCTION ENV')
+	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.get('/', (req,res) => {
+		res.sendFile(path.join(__dirname, '../build/'))
+	})
+	
+
+}
+
+//Express App Routing
+app.use('/auth,', require('./auth'))
+
+app.use(function(err, req, res, next){
+	console.log('==== ERROR ====')
+	console.error(err.stack)
+	res.status(500)
+})
+
 // Listen on port 3000
 app.listen(PORT, function() {
   console.log("App running on " + PORT + "!");
